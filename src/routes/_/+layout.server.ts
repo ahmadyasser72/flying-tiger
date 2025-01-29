7917e3a8e58aaf9369004d97237b19ce8e3c49a9
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-01-29 18:44:05 +0800

    tambah dependensi authorize pada route khusus admin

--- src/routes/_/+layout.server.ts
+++ src/routes/_/+layout.server.ts
@@ -1,6 +1,7 @@
 import type { LayoutServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = async ({ locals, depends }) => {
+	depends('admin:authorize');
 	if (!locals.authorized) error(401, { message: 'Halaman ini khusus untuk admin!' });
 };
