f247d1ebb5b5944233bd07b6fe4889d0a4f006e8
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-02-02 22:54:49 +0800

    tambah progress indicator saat navigasi

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { Toaster } from '$lib/components/ui/sonner';
 	import '../app.css';
 	import type { LayoutData } from './$types';
+	import { ProgressBar } from '@prgm/sveltekit-progress-bar';
 	import type { Snippet } from 'svelte';
 	import { toast } from 'svelte-sonner';
 	import { getFlash } from 'sveltekit-flash-message';
@@ -14,11 +15,17 @@
 	$effect(() => {
 		if ($flash !== undefined) toast[$flash.type]($flash.message);
 	});
+
+	let progressBar = $state<ReturnType<typeof ProgressBar>>();
 </script>
 
+<ProgressBar class="text-current" bind:this={progressBar} />
 <Toaster />
 
-<div class="container flex min-h-screen flex-col">
+<div
+	class="container flex min-h-screen flex-col bg-white"
+	class:brightness-90={progressBar?.getState().running}
+>
 	<header
 		class="flex flex-wrap items-center justify-center gap-4 border-b px-8 py-4 text-center sm:justify-end"
 	>
